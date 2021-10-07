659abf581b5759169fdc58f7dd97dccc01ebef83
Jim Wardlaw <jim@wearesauce.io>
2021-10-07 17:23:49 +0100

    Build tweaks

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,39 @@
-'use strict';
+"use strict";
 
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require("ember-cli/lib/broccoli/ember-app"),
+  autoprefixer = require("autoprefixer"),
+  tailwind = require("tailwindcss"),
+  tailwindConfig = "tailwind.config.js",
+  isProduction = EmberApp.env() === "dont-purge-yet-production",
+  purgeCSS = {
+    module: require("@fullhuman/postcss-purgecss"),
+    options: {
+      content: [
+        // add extra paths here for components/controllers which include tailwind classes
+        "./app/index.html",
+        "./app/templates/**/*.hbs",
+        "./app/components/**/*.hbs",
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    },
+  };
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    // Post CSS Stuff
+    postcssOptions: {
+      compile: {
+        enabled: true,
+        plugins: [
+          {
+            module: autoprefixer,
+            options: {},
+          },
+          // tailwind(tailwindConfig),
+          // ...(isProduction ? [purgeCSS] : []),
+        ],
+      },
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -18,7 +47,7 @@ module.exports = function(defaults) {
   // If the library that you are including contains AMD or ES6
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  // along with the exports of each module as its value.\\
 
   return app.toTree();
 };
